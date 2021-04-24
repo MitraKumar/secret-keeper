bcb8bdf9d958e44759a05d8d2ba1b91ddf0117ca
kkmitra <kaushik.mitra@innoaft.com>
2021-04-24 14:41:59 +0530

    Added redux starter code

---
+++ src/store/reducers/index.ts
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+
+const initialState = {};
+
+const mainReducer =  (state = initialState, action:any) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default mainReducer;
